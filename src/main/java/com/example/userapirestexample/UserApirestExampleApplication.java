34b655182bf8f38238eb9a36aba085ca600653bc
nayibimartin <nayibi.martin@gmail.com>
2021-08-31 11:22:12 -0400

    Creando proyecto

---
+++ src/main/java/com/example/userapirestexample/UserApirestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.userapirestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApirestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApirestExampleApplication.class, args);
+	}
+
+}
