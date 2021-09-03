12eeeb5a8793b894f003bfecefad808c466fe2fd
nayibimartin <nayibi.martin@gmail.com>
2021-09-03 14:32:04 -0400

    User-3 Feature update user

--- src/main/java/com/example/userapirestexample/domain/repository/UserRepository.java
+++ src/main/java/com/example/userapirestexample/domain/repository/UserRepository.java
@@ -3,5 +3,7 @@ package com.example.userapirestexample.domain.repository;
 import com.example.userapirestexample.domain.entity.User;
 
 public interface UserRepository {
-	public User create(User user);
+
+	User create(User user);
+
 }
--- src/main/java/com/example/userapirestexample/infraestructure/persistence/UserRepositoryJPA.java
+++ src/main/java/com/example/userapirestexample/infraestructure/persistence/UserRepositoryJPA.java
@@ -1,10 +1,11 @@
 package com.example.userapirestexample.infraestructure.persistence;
 
 import com.example.userapirestexample.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepositoryJPA extends PagingAndSortingRepository<User,Integer> {
+public interface UserRepositoryJPA extends JpaRepository<User,Integer> {
 
 }
